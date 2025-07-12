92e48874c65970fb13aee8ec65b92367eb4625ed
huerta-kevin <huerta.s.kevin@icloud.com>
2025-07-12 16:25:14 -0700

    Add HomePage and routes

--- app/client/src/App.js
+++ app/client/src/App.js
@@ -1,11 +1,15 @@
 import { Routes, Route } from 'react-router-dom';
+import HomePage from "./pages/HomePage";
 import CategoryPage from './pages/CategoryPage';
 
 export default function App() {
   return (
       <Routes>
-        <Route path="/" element={<CategoryPage />} />
-        {/* future routes here */}
+          <Route path="/" element={<HomePage />} />
+          <Route path="/category" element={<CategoryPage />} />
+          {/* later: <Route path="/cart" element={<CartPage />} /> */}
+          {/* later: <Route path="/orders" element={<OrdersPage />} /> */}
+          {/* later: <Route path="/login" element={<AuthPage />} /> */}
       </Routes>
   );
 }
